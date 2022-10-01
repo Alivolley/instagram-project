22c9275020e348d3a49d83c282266f120d00ddb5
Ali <alicryptovolley@gmail.com>
2022-10-01 11:27:15 +0330

    project created

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+   return (
+      <div>
+         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit dolores perspiciatis vitae explicabo, animi qui ut incidunt repellat saepe deserunt alias obcaecati
+         deleniti, velit accusamus aspernatur hic veritatis quis magni.
+      </div>
+   );
+}
