b4c356b52004ddbe34a7077f28a70348b83dbc68
Ali <alicryptovolley@gmail.com>
2023-01-09 14:17:32 +0330

    baseURL debugg

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-   baseURL: "http://instagramdjango.pythonanywhere.com",
+   baseURL: "https://instagramdjango.pythonanywhere.com",
    headers: {
       "Content-Type": "application/json",
    },
